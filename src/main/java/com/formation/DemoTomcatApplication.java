f7819cafd9ceb333c3e724326e2c7a4f1a0d0add
mm167 <mm167@example.com>
2024-05-27 18:17:39 +0200

    Adding finalname

--- src/main/java/com/formation/DemoTomcatApplication.java
+++ src/main/java/com/formation/DemoTomcatApplication.java
@@ -2,12 +2,13 @@ package com.formation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoTomcatApplication {
+public class DemoTomcatApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoTomcatApplication.class, args);
